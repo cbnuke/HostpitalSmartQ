eb01b97b51fdc72e588dbe31860f76e8cc5e2558
cbnuke <jimmy29922@gmail.com>
2017-01-13 00:04:07 +0700

    map

--- application/views/map.php
+++ application/views/map.php
@@ -6,7 +6,7 @@
         <meta name = "apple-mobile-web-app-status-bar-style" content = "black"/>
 
 
-        <meta name = "viewport" content = "width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
+        <meta name = "viewport" content = "width=device-width; initial-scale=0.5; maximum-scale=1.0; user-scalable=0;"/>
         <style type = "text/css">
             img {border-width: 0}
             * {font-family:'Lucida Grande', sans-serif;
@@ -16,7 +16,7 @@
     <body>
 
         <div style = "background: url('<?= img_url().'map-eng.png'?>') no-repeat; width: 960px; height: 540px;"></div>
-        <div name = "man" style = "position: absolute; top: <?php echo $man_dep_lat ?>px; left: <?php echo $man_dep_long ?>px"><img src = "<?= img_url().'Man.png'?>"></div>
+        <div name = "man" style = "position: absolute; top: <?php echo $man_dep_lat ?>px; left: <?php echo $man_dep_long ?>px"><img src = "<?= img_url().'Man2.png'?>"></div>
         <div name = "flag" style = "position: absolute; top: <?php echo $flag_dep_lat ?>px; left: <?php echo $flag_dep_long ?>px"><img src = "<?= img_url().'Flag.png'?>"></div>
 
     </body>
