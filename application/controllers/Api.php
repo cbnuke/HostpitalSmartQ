8b4eba3aeb9527a001a742727ed1907dcb4bba2f
CBNUKE <jimmy29922@gmail.com>
2017-01-12 17:47:31 +0700

    api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -14,6 +14,36 @@ class Api extends CI_Controller {
         show_404();
     }
 
+    public function login() {
+        if ($this->input->method(TRUE) === 'POST') {
+            $hn = $this->input->post('HN');
+            $pw = $this->input->post('PW');
+            $token = $this->input->post('Token');
+
+            //Check patient
+            $flag = FALSE;
+            $query = $this->db->get_where('patient', array('pat_hn' => $hn, 'pat_pass' => $pw));
+            if ($query->num_rows() == 1) {
+                $flag = TRUE;
+                $data_update = array(
+                    'pat_token' => $token,
+                );
+                $this->db->where('pat_hn', $hn);
+                $this->db->where('pat_pass', $pw);
+                $this->db->update('patient', $data_update);
+            }
+
+            $data = array(
+                'data' => 'login',
+                'status' => ($flag) ? 'success' : 'fail'
+            );
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        } else {
+            show_404();
+        }
+    }
+
     public function patient() {
         if ($this->input->method(TRUE) === 'POST') {
             $hn = $this->input->post('hn');
